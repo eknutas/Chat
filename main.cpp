c48db989f579aa450b506e24a511872319eb6d62
eknutas <109955298+eknutas@users.noreply.github.com>
2023-03-08 10:14:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/* main.cpp Console_chat */
+
+#include "Chat.h"
+
+int main()
+{
+    Chat chat;
+    while (true)
+    {
+        char mode;
+        std::cout << "\nEnter 1 to enter the chat \n";
+        std::cout << "Enter 2 to register in the chat \n";
+        std::cout << "Enter 3 to exit the program \n";
+        std::cin >> mode;
+        try
+        {
+            switch (mode)
+            {
+            case '1':   //Entering the chat
+                chat.logIn();
+                break;
+            case '2':   //New User Registration
+                chat.checkIn();
+                break;
+            case '3':   //Exiting the program
+                std::cout << "Goodbye!" << '\n';
+                return 0;
+            default:
+                throw "Wrong enter, please re-enter!";
+            }
+        }
+        catch (const char* exception)
+        {
+            std::cerr << "Error: " << exception << '\n';
+        }
+    }
+}
